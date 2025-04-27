f57466b1f3350e76f9d829b08917c9cdaef91776
HarryBae1011 <06harry@naver.com>
2025-04-27 09:28:45 +0900

    mission/#10

---
+++ src/main/java/umc/study/domain/Review.java
@@ -0,0 +1,34 @@
+package umc.study.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+import umc.study.domain.common.BaseEntity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class Review extends BaseEntity {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "shop_id")
+    private Shop shop;
+
+    private String reviewText;
+
+    private Float star;
+
+    @OneToMany(mappedBy = "review", cascade = CascadeType.ALL)
+    private List<ReviewImage> reviewImageList = new ArrayList<>();
+}
---
+++ src/main/java/umc/study/domain/Shop.java
@@ -0,0 +1,37 @@
+package umc.study.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+import umc.study.domain.common.BaseEntity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class Shop extends BaseEntity {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "region_id")
+    private Region region;
+
+    @Column(length = 20)
+    private String category;
+
+    private String shopInfo;
+
+    @OneToOne(mappedBy = "shop", fetch = FetchType.LAZY)
+    private BusinessHours businessHours;
+
+    @OneToMany(mappedBy = "shop", cascade = CascadeType.ALL)
+    private List<Review> reviewList = new ArrayList<>();
+
+    @OneToMany(mappedBy = "shop", cascade = CascadeType.ALL)
+    private List<Mission> missionList = new ArrayList<>();
+}
